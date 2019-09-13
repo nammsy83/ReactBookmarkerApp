d9fb514220e97b4b7d181f5d28067b2c09cc19e9
Namrata Subhedar <nammsy@gmail.com>
2019-09-13 18:42:27 +0530

    Updates

---
+++ precache-manifest.e184aef421fa1c7b91545a65baf4c0a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7004adf29b722df6136e8018cd66801d",
+    "url": "/ReactBookmarkerApp/index.html"
+  },
+  {
+    "revision": "bdd6a3a116c1f6bbe660",
+    "url": "/ReactBookmarkerApp/static/css/main.b3ef4d89.chunk.css"
+  },
+  {
+    "revision": "da2ca2e8dd8d7d6e80e5",
+    "url": "/ReactBookmarkerApp/static/js/2.6953047f.chunk.js"
+  },
+  {
+    "revision": "bdd6a3a116c1f6bbe660",
+    "url": "/ReactBookmarkerApp/static/js/main.7cc4b96b.chunk.js"
+  },
+  {
+    "revision": "31f11432129683ad05cb",
+    "url": "/ReactBookmarkerApp/static/js/runtime~main.29bfd97e.js"
+  }
+]);
